b982176f649f1121536ab77e8c88ff9c10ffaf0b
CarlosFULLHD <cninareynaga@gmail.com>
2023-09-27 16:07:55 -0400

    subiendo minimos cambios

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# spring-todolist-backend
+# backend-todolist
+
+## Requerimientos
+
+Para correr este proyecto debe tener instalado:
+
+- Java 11
+- Maven 3.8.4
+
+## Para compilar el proyecto
+Asegurese de estar en la carpeta del pom.xml
+
+```
+mvn clean install
+```
+
+## Ejecución
+
+```
+mvn spring-boot:run
+```
+
+## Instalación de la Base de Datos
+
+1. Hacer correr una instancia Postgres en docker
+
+```
+docker run --name todolistdb -e POSTGRES_PASSWORD=123456 -p 5432:5432 -d postgres:16
+```
+
+2. Me conecto a la DB database:"todolistdb" password:"123456"
+
+   Conexion mediante DataGrip
+```
+Host: localhost
+Posrt: 5432
+Authenticacion: User & Password
+User: postgres
+Password: 123456
+Database: postgres
+```
+Se hace "Test Connection con esas credenciales y se conecta con Datagrip"
+
+3. Creamos la Base de Datos de la todolistdb
+
+```
+CREATE DATABASE todolistdb;
+```
+
+4. Creamos la Base de Datos de la todolistdb.
+
+```
+ \c todolistdb;
+```
+
+5. Ejecutan el script "init.sql" de la carpeta "database".
+
+---
+
+Nota: Una vez hecho el paso 1, para volver a correr el contenedor de docker(que tiene la base de datos)
+
+```
+docker start todolistdb
+```
---
+++ src/main/java/bo/ucb/edu/todolist/entity/User.java
@@ -0,0 +1,56 @@
+package bo.ucb.edu.todolist.entity;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "username", unique = true, nullable = false, length = 255)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    // Getters and setters
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                '}';
+    }
+}

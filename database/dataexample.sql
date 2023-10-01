30e764c0edd98939cc5a0e76c0fb6c172dc47250
CarlosFULLHD <cninareynaga@gmail.com>
2023-10-01 07:09:39 -0400

    añadiendo datos de ejemplo para bdd

--- database/dataexample.sql
+++ database/dataexample.sql
@@ -1,27 +1,50 @@
--- Insertar datos de ejemplo en la tabla 'users'
+-- Insertar datos de ejemplo en la tabla users
 INSERT INTO users (username, password_hash)
 VALUES
     ('usuario1', 'password123'),
-    ('usuario2', 'secret456');
+    ('usuario2', 'password2'),
+    ('usuario3', 'password3');
 
--- Insertar datos de ejemplo en la tabla 'labels'
+-- Insertar datos de ejemplo en la tabla labels
 INSERT INTO labels (label_name)
 VALUES
     ('Personal'),
     ('Trabajo'),
     ('Estudio');
 
--- Insertar datos de ejemplo en la tabla 'tasks' (asociadas a usuarios)
+-- Insertar datos de ejemplo en la tabla tasks y sus relaciones en task_labels
 INSERT INTO tasks (task_name, due_date, status, user_id)
 VALUES
-    ('Tarea 1', '2023-10-01', true, 1),
-    ('Tarea 2', '2023-10-02', false, 1),
-    ('Tarea 3', '2023-10-03', false, 2);
+    ('Tarea1 de Usuario1', NOW(), FALSE, 1),
+    ('Tarea2 de Usuario1', NOW(), FALSE, 1),
+    ('Tarea3 de Usuario1', NOW(), FALSE, 1),
+    ('Tarea1 de Usuario2', NOW(), FALSE, 2),
+    ('Tarea2 de Usuario2', NOW(), FALSE, 2),
+    ('Tarea3 de Usuario2', NOW(), FALSE, 2),
+    ('Tarea1 de Usuario3', NOW(), FALSE, 3),
+    ('Tarea2 de Usuario3', NOW(), FALSE, 3),
+    ('Tarea3 de Usuario3', NOW(), FALSE, 3);
 
--- Insertar datos de ejemplo en la tabla 'task_labels' (asociación de tareas a etiquetas)
+-- Insertar relaciones many-to-many en la tabla task_labels
 INSERT INTO task_labels (task_id, label_id)
 VALUES
-    (1, 1), -- Tarea 1 pertenece a la etiqueta Personal
-    (2, 2), -- Tarea 2 pertenece a la etiqueta Trabajo
-    (3, 1), -- Tarea 3 pertenece a la etiqueta Personal
-    (3, 3); -- Tarea 3 pertenece a la etiqueta Estudio
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 1),
+    (5, 2),
+    (6, 3),
+    (7, 1),
+    (8, 2),
+    (9, 3);
+
+
+   -- SELECT FROM
+   select * from users;
+   select * from tasks;
+   select * from labels;
+   select * from task_labels;
+
+   INSERT INTO users (username, password_hash)
+   VALUES
+       ('usuarionuevo', '1234');
